b52ac2ac86465a4fc675bf8d9aa3afe5eacb0679
Rakib Ahmed <rakib987@gmail.com>
2021-07-07 10:34:55 -0400

    Created Fully Responsive Application, Will Now Add Functionality

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 console.log(this);
-
+// Test to see all elements with the class name .content
+console.log(document.querySelectorAll(".content"));
 // Global Variables
 var win = 0;
-var options = ["Rock", "Paper", "Scissors"];
+var options = ["R", "P", "S"];
 var index = Math.floor(Math.random() * options.length);
 var computerChoice = options[index];
+
+// Testing Console Log // Comment it out when not using
+// console.log(index);
 
 // playGame fucntion
 function playGame() {
     // Prompt user for choice of Rock Paper or Scissors (R, P, S)
-    var userChoice = prompt("Enter Choice of Rock Paper or Scissors");
-    
+    var userChoice = prompt("Enter Choice of Rock Paper or Scissors", "R, P, or S");
+    console.log(index);
     if (!userChoice) {
      return;
     }
---
+++ copyrightYear.js
@@ -0,0 +1,4 @@
+// Code to getFullYear from User's Device
+var today = new Date();
+var year = today.getFullYear();
+document.write(year);
